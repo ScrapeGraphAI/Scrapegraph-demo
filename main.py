fd4f99f9c3c0335dd46b5998327fe56640fa4da5
VinciGit00 <mvincig11@gmail.com>
2024-02-21 18:07:35 +0100

    add new UI

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import base64
 import streamlit as st
 
 st.title("Scrapegraph-ai")
@@ -20,3 +21,25 @@ if st.button("Run th program", type="primary"):
     st.write('DO something')
 else:
     st.write('')
+
+left_co2,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,cent_co2,last_co2 = st.columns([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])
+
+with cent_co2:
+    discord_link = "https://discord.gg/DujC7HG8"
+    discord_logo = base64.b64encode(open("discord.png", "rb").read()).decode()
+    st.markdown(
+        f"""<a href="{discord_link}" target="_blank">
+        <img src="data:image/png;base64,{discord_logo}" width="25">
+        </a>""",
+        unsafe_allow_html=True,
+    )
+
+with last_co2:
+    discord_link = "https://github.com/VinciGit00/Scrapegraph-ai"
+    discord_logo = base64.b64encode(open("github.png", "rb").read()).decode()
+    st.markdown(
+        f"""<a href="{discord_link}" target="_blank">
+        <img src="data:image/png;base64,{discord_logo}" width="25">
+        </a>""",
+        unsafe_allow_html=True,
+    )
